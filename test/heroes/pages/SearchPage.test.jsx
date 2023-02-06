b8463153631fb592859d7b8115de0e6c5dd27780
Steven Reina <nelson.reina132@gmail.com>
2023-02-06 15:01:19 -0500

    correccion del package.json

--- test/heroes/pages/SearchPage.test.jsx
+++ test/heroes/pages/SearchPage.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 const { MemoryRouter } = require("react-router-dom");
 const { SearchPage } = require("../../../src/heroes/pages/SearchPage");
 
-// jest.mock('../../../node_modules/query-string', () => '');
+jest.mock('../../../node_modules/query-string', () => '');
 
 describe('Pruebas en <SearchPage/>', () => {
 	
